fb5f180328763b6cfa6f6c942921cad0ec667f29
caique@jamsoft.com.br <caique@jamsoft.com.br>
2021-03-14 07:07:12 -0300

    Adicionar arquivos de projeto.

---
+++ MeuBanco/frmDeposito.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace MeuBanco
+{
+    partial class frmDeposito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSaque = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSaque
+            // 
+            this.txtSaque.Location = new System.Drawing.Point(72, 47);
+            this.txtSaque.Name = "txtSaque";
+            this.txtSaque.Size = new System.Drawing.Size(100, 20);
+            this.txtSaque.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Valor";
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(190, 45);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(107, 24);
+            this.btnSacar.TabIndex = 2;
+            this.btnSacar.Text = "Depositar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            // 
+            // frmDeposito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 115);
+            this.Controls.Add(this.btnSacar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSaque);
+            this.Name = "frmDeposito";
+            this.Text = "frmSaque";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSaque;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSacar;
+    }
+}
---
+++ MeuBanco/frmPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace MeuBanco
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTransferencia = new System.Windows.Forms.Button();
+            this.btnDeposito = new System.Windows.Forms.Button();
+            this.btnSaque = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTransferencia
+            // 
+            this.btnTransferencia.Location = new System.Drawing.Point(543, 186);
+            this.btnTransferencia.Name = "btnTransferencia";
+            this.btnTransferencia.Size = new System.Drawing.Size(221, 66);
+            this.btnTransferencia.TabIndex = 0;
+            this.btnTransferencia.Text = "Transferência";
+            this.btnTransferencia.UseVisualStyleBackColor = true;
+            this.btnTransferencia.Click += new System.EventHandler(this.btnTransferencia_Click);
+            // 
+            // btnDeposito
+            // 
+            this.btnDeposito.Location = new System.Drawing.Point(287, 186);
+            this.btnDeposito.Name = "btnDeposito";
+            this.btnDeposito.Size = new System.Drawing.Size(221, 66);
+            this.btnDeposito.TabIndex = 1;
+            this.btnDeposito.Text = "Depósito";
+            this.btnDeposito.UseVisualStyleBackColor = true;
+            this.btnDeposito.Click += new System.EventHandler(this.btnDeposito_Click);
+            // 
+            // btnSaque
+            // 
+            this.btnSaque.Location = new System.Drawing.Point(31, 186);
+            this.btnSaque.Name = "btnSaque";
+            this.btnSaque.Size = new System.Drawing.Size(221, 66);
+            this.btnSaque.TabIndex = 2;
+            this.btnSaque.Text = "Saque";
+            this.btnSaque.UseVisualStyleBackColor = true;
+            this.btnSaque.Click += new System.EventHandler(this.btnSaque_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaque);
+            this.Controls.Add(this.btnDeposito);
+            this.Controls.Add(this.btnTransferencia);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTransferencia;
+        private System.Windows.Forms.Button btnDeposito;
+        private System.Windows.Forms.Button btnSaque;
+    }
+}
+
